9fa008069aad5af518d72defc93550298dfaabe0
JJ <macbook@MacBook-di-Jacopo-Tupac-Santaiti.local>
2013-07-06 11:40:11 +0200

    primo commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiaApp::Application.config.secret_token = '6241666b09b6e6479e9cdc69498adcb04fe1a38202e8b0682bca46ef46d746130f592f782f8b9fd7d5e6f60d1ac67114666f5d9185401b398b053ff88164e4e9'
